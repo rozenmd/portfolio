406c163bc5ccb225ac004ed54d6b8c26a87f36dc
Max Rozen <mroz2624@uni.sydney.edu.au>
2016-01-25 14:22:20 +1100

    Add styling, config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -246,7 +246,7 @@ INSTALLED_APPS = (
     "mezzanine.blog",
     "mezzanine.forms",
     "mezzanine.galleries",
-    "mezzanine.twitter",
+    # "mezzanine.twitter",
     # "mezzanine.accounts",
     # "mezzanine.mobile",
 )
